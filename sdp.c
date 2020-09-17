753483f24892d3c37b4263e2c00e4bbc9c12db05
Sanjat Chand <chandsanjat@gmail.com>
2020-09-17 13:35:53 +0530

    Create sdp.c

---
+++ sdp.c
@@ -0,0 +1,14 @@
+void printKMax(int arr[], int n, int k) 
+{ 
+    int j, max; 
+  
+    for (int i = 0; i <= n - k; i++) { 
+        max = arr[i]; 
+  
+        for (j = 1; j < k; j++) { 
+            if (arr[i + j] > max) 
+                max = arr[i + j]; 
+        } 
+        printf("%d ", max); 
+    } 
+}
